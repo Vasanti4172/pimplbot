a8da2a04dbbfe77c486bda568321a12d4b648dfe
Vasanti4172 <vasanthivanapalli2@gmail.com>
2025-03-25 18:17:34 +0530

    Integrated React app from Vercel

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
-st.title("PimplBot UI")
-st.components.v1.iframe("https://pimplbot-f6nhxvv0q-vanapalli-vasanti-lakshmis-projects.vercel.app", height=600)
+st.set_page_config(page_title="PimplBot", layout="wide")
 
+st.title("PimplBot UI")
+
+# Embed React App from Vercel
+st.components.v1.iframe("https://pimplbot-f6nhxvv0q-vanapalli-vasanti-lakshmis-projects.vercel.app", height=600, scrolling=True)
